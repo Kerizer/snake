f8c28ce66815714211a9eca5a8e79852d979d33a
Pasha <vikkaming@gmail.com>
2017-07-06 22:05:26 +0300

    snake mock

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
  * Created by Pasha on 06-Jul-17.
  */
 
+let snake = new Snake();
 let root = document.querySelector('#root');
 
 let viewPort = document.createElementNS('http://www.w3.org/2000/svg','svg');
@@ -15,7 +16,7 @@ viewPort.setAttribute('width', (ROWS*RECT_SIZE).toString());
 let rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
 rect.setAttribute('width', RECT_SIZE);
 rect.setAttribute('height', RECT_SIZE);
-rect.setAttribute('style', "fill:rgb(255,255,255);stroke-width:1;stroke:rgb(0,0,0)");
+
 
 // We have a ROWS x COLS matrix filled by `rect` elements
 // let rectMatrix = new Array(COLS).fill(new Array(ROWS).fill(rect.cloneNode(false)));
@@ -23,13 +24,16 @@ rect.setAttribute('style', "fill:rgb(255,255,255);stroke-width:1;stroke:rgb(0,0,
 
 
 // console.log(rectMatrix);
-let i, j, row, col;
-for (i=0; i<ROWS; i++) {
+let x, y, row, col;
+for (x=0; x<ROWS; x++) {
 	row = document.createElementNS('http://www.w3.org/2000/svg','svg');
-	for (j=0; j<ROWS; j++) {
+	for (y=0; y<ROWS; y++) {
 		col = document.createElementNS('http://www.w3.org/2000/svg','svg');
-		col.setAttribute('x', (j*RECT_SIZE).toString());
-		col.setAttribute('y', (i*RECT_SIZE).toString());
+		col.setAttribute('x', (x*RECT_SIZE).toString());
+		col.setAttribute('y', (y*RECT_SIZE).toString());
+
+		rect.setAttribute('style', `fill:rgb(${snake.check(x, y) ? '0, 0, 0' : '255,255,255'});stroke-width:1;stroke:rgb(0,0,0)`);
+
 		col.appendChild(rect.cloneNode(false));
 		viewPort.appendChild(col);
 	}
--- src/snake.js
+++ src/snake.js
@@ -4,7 +4,38 @@
  */
 
 class Snake {
-	constructor(x, y) {
+	constructor() {
+		this.direction = 'RIGHT';
+		this.collection = [
+			{x:1, y:0},
+			{x:2, y:0},
+			{x:3, y:0}
+		];
+		this.tick();
+	}
+
+	check(x, y) {
+		// next line doesn't work because two object`s are always non-equal =(
+		// return !!~this.collection.indexOf({x,y});
+
+		// as soon as acc will become true it will always return true
+		// it will become true as soon, as the first coincidence will be detected BUT it won`t exit from the method, so:
+		// TODO: rewrite it using 'for' can be a good idea
+		return this.collection.reduce((acc, item)=>(acc || item.x === x && item.y === y), false);
+	}
+
+	tick() {
+		let collection = [...this.collection];
+		collection.splice(0, 1);
+		this.collection = [...collection, {x:this.collection[this.collection.length-1].x+1, y:0}];
+		console.log(this.collection);
+		setTimeout(()=>{
+			this.tick();
+			// TODO:fire rerender here
+		}, 1000);
+	}
+
+	changeDirection(direction) {
 
 	}
 }
